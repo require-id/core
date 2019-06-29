5bfd05712205346f5d7215df935a8cf1f97c7739
Lauri <lauri.vuorela@gmail.com>
2019-06-29 20:57:52 +0200

    untested store code

--- app/src/handlers/backup/store.py
+++ app/src/handlers/backup/store.py
@@ -1,9 +1,25 @@
 import asyncio
 import json
+import os
+import shutil
+
+import boto3
+
+BUCKET = 'require-id-bucket'
+LOCAL_DIRECTORY = '/backups'
+
+
+def s3upload(key, data):
+    client = boto3.client('s3')
+    client.put_object(
+        Bucket=BUCKET,
+        Key=f'backups/{key}',
+        Body=data
+    )
 
 
 async def handler(event, context):
-    aws_request_id = context.aws_request_id
+    # aws_request_id = context.aws_request_id
     method = event.get('httpMethod')
     body = event.get('body')
 
@@ -13,9 +29,29 @@ async def handler(event, context):
             'body': json.dumps({'message': 'Method Not Allowed'})
         }
 
+    json_data = json.loads(body)
+    identifier = json_data.get('identifier')
+    encrypted_data = json_data.get('data')
+    if isinstance(encrypted_data, str):
+        encrypted_data_bytes = encrypted_data.encode('utf-8')
+    if BUCKET:
+        try:
+            s3upload(key=identifier, data=encrypted_data_bytes)
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'message': 'Internal Server Error', 'error': f'{e}'})
+            }
+    else:
+        backup_file_path = os.path.join(LOCAL_DIRECTORY, identifier)
+        if os.path.isfile(backup_file_path):
+            shutil.copyfile(backup_file_path, f'{backup_file_path}.lastver')
+        with open(backup_file_path) as backup_file:
+            backup_file.write(encrypted_data_bytes)
+
     return {
         'statusCode': 200,
-        'body': f'{method} – backup.store: {aws_request_id}'
+        'body': json.dumps({'message': 'Data backed up'})
     }
 
 
