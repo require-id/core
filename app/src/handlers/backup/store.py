22ed96d3aec4fb875ce6d4d3929222f87e355ed6
Lauri <lauri.vuorela@gmail.com>
2019-06-29 21:16:36 +0200

    some refactoring

--- app/src/handlers/backup/store.py
+++ app/src/handlers/backup/store.py
@@ -9,13 +9,23 @@ BUCKET = 'require-id-bucket'
 LOCAL_DIRECTORY = '/backups'
 
 
-def s3upload(key, data):
+def s3_backup(key, data):
+    # SMELL: don't really know if this works credential wise
     client = boto3.client('s3')
     client.put_object(
         Bucket=BUCKET,
         Key=f'backups/{key}',
         Body=data
     )
+
+
+def local_backup(identifier, encrypted_data_bytes):
+    backup_file_path = os.path.join(LOCAL_DIRECTORY, identifier)
+    if os.path.isfile(backup_file_path):
+        shutil.copyfile(backup_file_path, f'{backup_file_path}.lastver')
+
+    with open(backup_file_path) as backup_file:
+        backup_file.write(encrypted_data_bytes)
 
 
 async def handler(event, context):
@@ -34,20 +44,16 @@ async def handler(event, context):
     encrypted_data = json_data.get('data')
     if isinstance(encrypted_data, str):
         encrypted_data_bytes = encrypted_data.encode('utf-8')
-    if BUCKET:
-        try:
-            s3upload(key=identifier, data=encrypted_data_bytes)
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'message': 'Internal Server Error', 'error': f'{e}'})
-            }
-    else:
-        backup_file_path = os.path.join(LOCAL_DIRECTORY, identifier)
-        if os.path.isfile(backup_file_path):
-            shutil.copyfile(backup_file_path, f'{backup_file_path}.lastver')
-        with open(backup_file_path) as backup_file:
-            backup_file.write(encrypted_data_bytes)
+    try:
+        if BUCKET:
+                s3_backup(key=identifier, data=encrypted_data_bytes)
+        else:
+            local_backup(identifier, encrypted_data_bytes)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal Server Error', 'error': f'{e}'})
+        }
 
     return {
         'statusCode': 200,
