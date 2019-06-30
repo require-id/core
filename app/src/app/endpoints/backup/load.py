b49e69982b4fde53d16a0b0c611de40b3fa9116b
Lauri <lauri.vuorela@gmail.com>
2019-06-30 19:57:36 +0200

    validation of input data

--- app/src/app/endpoints/backup/load.py
+++ app/src/app/endpoints/backup/load.py
@@ -1,11 +1,18 @@
 import json
+
 from app.shared import data
+from app.shared.utils import validate_hash
 
 
 async def handler(event, context, self_hosted_config=None):
     aws_request_id = context.aws_request_id
 
     seed_hash = event.get('queryStringParameters', {}).get('seedHash')
-    backup_data = await data.load(seed_hash, self_hosted_config, file_type='backup')
+    if not validate_hash(seed_hash):
+        return 400, json.dumps({'error': 'Invalid value for seedHash'})
 
-    return 200, json.dumps({'request_id': aws_request_id, 'backup_data': backup_data})
+    backup_data = await data.load(seed_hash, 'backup', self_hosted_config)
+    if backup_data:
+        return 200, json.dumps({'request_id': aws_request_id, 'backup_data': backup_data})
+
+    return 404, json.dumps({'request_id': aws_request_id, 'message': 'Backup data loaded.', 'backup_data': backup_data})
--- app/src/app/endpoints/backup/store.py
+++ app/src/app/endpoints/backup/store.py
@@ -1,5 +1,7 @@
 import json
+
 from app.shared import data
+from app.shared.utils import validate_base64, validate_hash
 
 
 async def handler(event, context, self_hosted_config=None):
@@ -7,10 +9,16 @@ async def handler(event, context, self_hosted_config=None):
 
     body = json.loads(event.get('body'))
     seed_hash = body.get('seedHash')
+
+    if not validate_hash(seed_hash):
+        return 400, json.dumps({'error': 'Invalid value for seedHash'})
+
     backup_data = body.get('data')
     if isinstance(backup_data, str):
         backup_data = backup_data.encode('utf-8')
+    if not validate_base64(backup_data):
+        return 400, json.dumps({'error': 'Data must be base64 endcoded.'})
 
-    await data.store(seed_hash, self_hosted_config, file_type='backup', data=backup_data)
+    await data.store(seed_hash, 'backup', backup_data, self_hosted_config)
 
-    return 200, f'backup.store: {aws_request_id}'
+    return 200, json.dumps({'request_id': aws_request_id, 'message': 'Backup data stored.'})
